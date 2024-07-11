ea34955bbe042d693d6576d3b4abeca279c36435
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-07-11 09:23:39 +0100

    ATO-692: Adds getPublicKey method
    We will need to derive the public key from the serrialized private key
    provided by our stub RP config, so have added this method to derive the
    public key from a given RSA private key. I've also added some tests to
    for this class.

--- src/main/java/uk/gov/di/utils/PrivateKeyReader.java
+++ src/main/java/uk/gov/di/utils/PrivateKeyReader.java
@@ -3,25 +3,44 @@ package uk.gov.di.utils;
 import java.io.IOException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
 import java.util.regex.Pattern;
 
 public class PrivateKeyReader {
     private String privateKey;
+    private KeyFactory kf;
 
     public PrivateKeyReader(String privateKey) {
-        this.privateKey = privateKey;
+        try {
+            this.privateKey = privateKey;
+            this.kf = KeyFactory.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public RSAPrivateKey get() {
         try {
-            var kf = KeyFactory.getInstance("RSA");
             return (RSAPrivateKey)
                     kf.generatePrivate(new PKCS8EncodedKeySpec(format(this.privateKey)));
-        } catch (InvalidKeySpecException | IOException | NoSuchAlgorithmException e) {
+        } catch (InvalidKeySpecException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public RSAPublicKey getPublicKey() {
+        try {
+            var privateKey = (RSAPrivateCrtKey) get();
+            RSAPublicKeySpec publicKeySpec =
+                    new RSAPublicKeySpec(privateKey.getModulus(), privateKey.getPublicExponent());
+            return (RSAPublicKey) kf.generatePublic(publicKeySpec);
+        } catch (InvalidKeySpecException e) {
             throw new RuntimeException(e);
         }
     }
