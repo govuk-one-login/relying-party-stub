66bc628a5288acb8571e6113deb9be3d3c861d6d
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-06-25 17:49:01 +0100

    ATO-2705: Add alternative domain overide
    Nimbusds gives us the ability to override[1] the domain used to fetch
    the provider metatdata, as passing in just the base url expects
    the domain == the issuer. When using our alternative domain that
    is not the case as the issuer is still the normal oidc value, not
    the alternative.
    
    Luckily nimbuds comes with the ability to override this, by passing
    in the domain as a second parameter, we will fetch from that domain
    and validate the issuer using the first value passed in
    
    [1] - https://bitbucket.org/connect2id/oauth-2.0-sdk-with-openid-connect-extensions/src/bbd0e837d1a42db51267c4520559e428dc2440ef/src/main/java/com/nimbusds/openid/connect/sdk/op/OIDCProviderMetadata.java#lines-1786

--- src/main/java/uk/gov/di/utils/Oidc.java
+++ src/main/java/uk/gov/di/utils/Oidc.java
@@ -60,6 +60,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
@@ -85,8 +86,17 @@ public class Oidc {
         this.providerMetadata = loadProviderMetadata(idpUrl);
         this.alternativeProviderMetadata =
                 Optional.ofNullable(relyingPartyConfig.alternativeBaseUrl())
-                        .map(this::loadProviderMetadata);
+                        .map((altDomain) -> this.loadProviderMetadata(relyingPartyConfig.opBaseUrl(), altDomain));
         this.privateKeyReader = new PrivateKeyReader(relyingPartyConfig.clientPrivateKey());
+    }
+
+    private OIDCProviderMetadata loadProviderMetadata(String expectedIssuer, String baseurl){
+        try {
+            return OIDCProviderMetadata.resolve(new Issuer(expectedIssuer), new URL(baseurl));
+        } catch (Exception e) {
+            LOG.error("Unexpected exception thrown when loading provider metadata", e);
+            throw new RuntimeException(e);
+        }
     }
 
     private OIDCProviderMetadata loadProviderMetadata(String baseUrl) {
